5b1a86cfffc302c9d9c51f0b69a55181c64668b9
yeonha99 <rhdqn1207@naver.com>
2022-06-04 21:32:45 +0900

    feat:이벤트 처리

--- src/router/index.js
+++ src/router/index.js
@@ -74,6 +74,15 @@ const routes = [{
         // which is lazy-loaded when the route is visited.
         component: () =>
             import ( /* webpackChunkName: "rabderview" */ '../views/RenderView.vue')
+    },
+    {
+        path: '/event',
+        name: 'event',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "event" */ '../views/EventView.vue')
     }
 ]
 
---
+++ src/views/EventView.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+    <button type="button" @click="increaseCounter">Add 1</button>
+    <p>The Count is : {{counter}}</p>
+    <select v-model="city" @change="changeSelect">
+        <option value="서울">서울</option>
+        <option value="부산">부산</option>
+        <option value="제주">제주</option>
+    </select>
+    <input type="text" v-model="textValue" @keyup.enter="showValue"/>
+    </div>
+</template>
+<script>
+export default {
+    components: {},
+    data(){
+        return {
+            counter: 0,
+            city: "",
+            textValue:""
+        }
+    },
+    setup() {
+        
+    },
+    create(){
+
+    },
+    mounted(){
+    },
+    unmounted() {
+        
+    },
+    methods: {
+        increaseCounter(){
+            this.counter=this.counter+1;
+        },
+        changeSelect(){
+            alert(this.city)
+        },
+        showValue(){
+            //tap,delete,esc,space,up,down,right,left
+            alert(this.textValue)
+        }
+
+    }
+}
+</script>
+<style scoped>
+
+</style>
