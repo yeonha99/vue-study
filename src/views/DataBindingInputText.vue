6cb9a8a624852ce86ad358603f0eb8a9838c886c
yeonha99 <rhdqn1207@naver.com>
2022-06-04 18:25:43 +0900

    feat:속성에 데이터 바인딩

--- src/views/DataBindingInputText.vue
+++ src/views/DataBindingInputText.vue
@@ -1,36 +1,18 @@
 <template>
-<div>
-    <!--
-    <select v-model="city">
-        <option value="02">서울</option>
-        <option value="21">부산</option>
-        <option value="063">제주</option>
-    </select>
-    <label><input type="checkbox" value="서울" v-model="checked">서울</label>
-    <label><input type="checkbox" value="부산" v-model="checked">부산</label>
-    <label><input type="checkbox" value="제주" v-model="checked">제주</label>
-    -->
-    <label> <input type="radio" v-bind:value="radio[0]" v-model="picked" />서울</label>
-    <label> <input type="radio" v-bind:value="radio[1]" v-model="picked" />부산</label>
-    <label> <input type="radio" v-bind:value="radio[2]" v-model="picked" />제주</label>
-    <br>
-    <span>{{picked}}</span>
-
-</div>
+    <div>
+        <img v-bind:src="url" style="height:100px;width:auto;">
+        <input type="text" v-model="textValue" />
+        <button type="button" v-bind:disabled="textValue==''">Click</button>
+    </div>
 </template>
 <script>
 export default {
     components: {},
     data(){
         return {
-            city: "063" ,
-            checked: [],
-            radio: ["서울","부산","제주"],
-            radio1: "서울",
-            radio2: "부산",
-            radio3: "제주",
-            picked: []
-        };
+            url: "https://kr.vuejs.org/images/logo.png",
+            textValue: ''
+        }
     },
     setup() {
         
