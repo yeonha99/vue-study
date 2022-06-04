02910361b741b5020fb3131629b96a2df20b92e3
yeonha99 <rhdqn1207@naver.com>
2022-06-04 21:10:34 +0900

    feat:render

--- src/router/index.js
+++ src/router/index.js
@@ -65,6 +65,15 @@ const routes = [{
         // which is lazy-loaded when the route is visited.
         component: () =>
             import ( /* webpackChunkName: "listview" */ '../views/ListView.vue')
+    },
+    {
+        path: '/render',
+        name: 'render',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "rabderview" */ '../views/RenderView.vue')
     }
 ]
 
