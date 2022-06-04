2f1c687cf0f4629b3f03048c9eca409dbd585f0b
yeonha99 <rhdqn1207@naver.com>
2022-06-04 21:47:46 +0900

    feat:Computed

--- src/router/index.js
+++ src/router/index.js
@@ -83,6 +83,15 @@ const routes = [{
         // which is lazy-loaded when the route is visited.
         component: () =>
             import ( /* webpackChunkName: "event" */ '../views/EventView.vue')
+    },
+    {
+        path: '/computed',
+        name: 'computed',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "computed" */ '../views/ComputedView.vue')
     }
 ]
 
---
+++ src/views/ComputedView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <input type="text" v-model="firstName" />
+        <input type="text" v-model="lastName" />
+        <h1>Hello, {{fullName}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    components: {},
+    computed:{
+        fullName(){
+            return this.firstName+this.lastName;
+        }
+    },
+    data(){
+        return {
+            firstName: '재석',
+            lastName: '유'
+        }
+    },
+    setup() {
+        
+    },
+    create(){
+
+    },
+    mounted(){
+    },
+    unmounted() {
+        
+    },
+    methods: {
+        getFullName(){
+            return this.firstName+this.lastName;
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
