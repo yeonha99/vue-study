22cf31494bf6374cda6fb3882bfdb1a6d46bc58f
yeonha99 <rhdqn1207@naver.com>
2022-06-04 16:44:39 +0900

    feat:입력폼 데이터 바인딩 - input text, number, textarea

---
+++ src/views/DataBindingInputText.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+<input type="text" v-model="valueModel" />
+<input type="number" v-model.number="valueModel2" />
+<textarea v-model="valueModel3"></textarea>
+</div>
+</template>
+<script>
+export default {
+    components: {},
+    data(){
+        return {
+            valueModel: 'South Korea',
+            valueModel2: 12,
+            valueModel3: '안녕하세요.'
+        };
+    },
+    setup() {
+        
+    },
+    create(){
+
+    },
+    mounted(){
+        console.log(this.valueModel2);
+    },
+    unmounted() {
+        
+    },
+    methods: {
+        
+    }
+}
+</script>
+<style scoped>
+
+</style>
