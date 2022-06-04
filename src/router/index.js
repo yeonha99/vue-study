f12d47b5488212d61fe468138767bea6eb7a0fc5
yeonha99 <rhdqn1207@naver.com>
2022-06-04 16:29:41 +0900

    feat:원시 HTML 데이터 바인딩

--- src/router/index.js
+++ src/router/index.js
@@ -37,7 +37,16 @@ const routes = [{
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () =>
-            import ( /* webpackChunkName: "basic" */ '../views/DataBinding.vue')
+            import ( /* webpackChunkName: "databinding" */ '../views/DataBinding.vue')
+    },
+    {
+        path: '/databindinghtml',
+        name: 'databindinghtml',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "databindinghtml" */ '../views/DataBindingHtml.vue')
     }
 ]
 
---
+++ src/views/DataBindingHtml.vue
@@ -0,0 +1,32 @@
+<template>
+<div v-html="htmlString">
+</div>
+</template>
+<script>
+export default {
+    components: {},
+    data(){
+        return {
+            htmlString: '<p style="color:red;">This is a red string.</p>'
+        };
+    },
+    setup() {
+        
+    },
+    create(){
+
+    },
+    mounted(){
+
+    },
+    unmounted() {
+        
+    },
+    methods: {
+        
+    }
+}
+</script>
+<style scoped>
+
+</style>
