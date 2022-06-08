9afe47c020f9234c6422cc52845c94918228fde3
yeonha99 <rhdqn1207@naver.com>
2022-06-08 22:04:39 +0900

    feat:Mock서버에 api 보내기

--- src/router/index.js
+++ src/router/index.js
@@ -110,6 +110,15 @@ const routes = [{
         // which is lazy-loaded when the route is visited.
         component: () =>
             import ( /* webpackChunkName: "watch" */ '../views/ExampleView.vue')
+    },
+    {
+        path: '/serverdata',
+        name: 'serverdata',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "serverdata" */ '../views/ServerData.vue')
     }
 ]
 
---
+++ src/views/ServerData.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+<button type="button" @click="getProductList"> 조회 </button> 
+</div>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+    components: {},
+    data(){
+        return {
+            productList: 'Hello world'
+        };
+    },
+    setup() {
+        
+    },
+    create(){
+
+    },
+    mounted(){
+
+    },
+    unmounted() {
+        
+    },
+    methods: {
+        async getProductList(){
+            this.productList=await this.api('https://05f0d9d6-c167-4db9-90fe-2a0ddc252dd8.mock.pstmn.io/productList','get',{});
+            console.log(this.productList);
+        }
+        ,
+        async api(url, method, data){
+            return (await axios({
+                method: method,
+                url: url,
+                data: data
+            }).catch(e =>{
+                console.log(e);
+            })
+            ).data;
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
