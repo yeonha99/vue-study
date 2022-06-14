072545b15b3761d8088a3191c74415ca33283404
yeonha99 <rhdqn1207@naver.com>
2022-06-14 20:52:35 +0900

    feat:부모 컴포넌트에서 자식 컴포넌트로 데이터 전달

---
+++ src/views/ChildComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        likes: {{likes}}
+    </div>
+    <div>isOk: {{isOk}}</div>
+    <div>author name:{{author.name}}</div>
+</template>
+<script>
+export default {
+     components: {},
+     props: {
+         likes:{
+              type: Number,
+              default: 0
+         },
+         isOk:{
+             type:Boolean,
+             default:false
+         },
+         commentIds:{
+             type: Array
+         },
+         author:{
+             type:Object
+         }
+     },
+    data(){
+        return {
+          sampleData: ''  
+        };
+    },
+    setup() {
+        
+    },
+    create(){
+
+    },
+    mounted(){
+        console.log(this.commentIds)
+    },
+    unmounted() {
+        
+    },
+    methods: {
+
+    }
+}
+</script>
+<style scoped>
+
+</style>
--- src/views/ExampleView.vue
+++ src/views/ExampleView.vue
@@ -1,18 +1,20 @@
 <template>
     <div>
-        <PageTitle title="부모컴포넌트에서 전송할 페이지 타이틀"/>
+        <PageTitle :title="title"/>
+        <ChildComponent :likes="likes" :isOk="ture" :commentIds="[1,5,2,3]" :author="author"/>
     </div>
 </template>
 <script>
 import PageTitle from '../components/PageTitle.vue';
+import ChildComponent from './ChildComponent.vue';
 
 export default {
-     components: {PageTitle},
+     components: {PageTitle,ChildComponent},
     data(){
         return {
-            name: '',
-            age: 0,
-            job: '' 
+            title: '부모컴포넌트에서 전송할 페이지 타이틀',
+            likes: 23,
+            author: {name:'홍길동', company:'회사이름'}
         }
     },
     setup() {
