03c7de5e9e19f33de2a4c039e875b82f1a18966b
yeonha99 <rhdqn1207@naver.com>
2022-06-17 21:33:06 +0900

    feat:Slot

--- src/router/index.js
+++ src/router/index.js
@@ -119,6 +119,15 @@ const routes = [{
         // which is lazy-loaded when the route is visited.
         component: () =>
             import ( /* webpackChunkName: "serverdata" */ '../views/ServerData.vue')
+    },
+    {
+        path: '/slot',
+        name: 'slot',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "serverdata" */ '../views/SlotUseModalLayout.vue')
     }
 ]
 
